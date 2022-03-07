173aed4030d481197750c0e63c15a0cf430cae58
sisackov <1123889+sisackov@users.noreply.github.com>
2022-03-07 21:49:09 +0200

    test added

---
+++ sim/test.js
@@ -0,0 +1,18 @@
+// king > gnome > elf > fire
+// lemon > name > easy > yes
+
+//"abc", "cdg", "gfe",
+//"ghi", "ija"
+
+// a > c > g > i, e > a
+
+const suf = {
+    g: [king],
+    e: [gnome, fire, name],
+    f: [elf],
+    n: [lemon],
+    y: [easy],
+    s: [yes],
+};
+
+// const pref = { k, g, e, f, l, n, y };
